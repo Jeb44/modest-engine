6f39f491ff0597fce4ee281911b9eeebe1812ee8
Jeb <gabriel.zimmermann98@gmail.com>
2019-04-23 11:00:41 +0200

    Added multiple .json files in .vscode, maybe .gitignore ignores them

--- engine/main.cpp
+++ engine/main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 
 int main(){
-
-
     std::cout << "Hello World!" << std::endl;
+
+    std::cout << std::endl << "Press any key to continue..." << std::endl;
+    getchar();
 }
