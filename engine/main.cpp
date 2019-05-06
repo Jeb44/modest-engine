aaeaa4cb90f9949bd1aaa43236459757db26446e
Jeb <gabriel.zimmermann98@gmail.com>
2019-05-06 23:30:45 +0200

    Started working with CMake. Current problem with including meConfig.h

--- engine/main.cpp
+++ engine/main.cpp
@@ -3,13 +3,20 @@
 #include "common/typedef.h"
 #include <assert.h>
 
-#include "subsystem/NewClass.h"
+//look up cmake tools
+#include "meConfig.h"
 
-int main(){	
-
-	// ...
-	NewClass _class;
+int main(int argc, char* argv[]){	
 	
+	if(argc < 2){
+		std::cout << argv[0] << " Version "
+					<< ME_VERSION_MAJOR << "."
+					<< ME_VERSION_MINOR << "."
+					<< ME_VERSION_PATCH
+					<< std::endl;
+		return 1;
+	}
+
 	std::cout << std::endl << "[END] Press any key to close program..." << std::endl;
 	getchar();
 
