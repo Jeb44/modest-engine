0dc11636d4dae51cd32d38b8f8abf858179fe588
Jeb <gabriel.zimmermann98@gmail.com>
2019-05-08 00:07:24 +0200

    Version 0.1.0 | CMake Support starts from here -> Test on Laptop aswell

--- engine/main.cpp
+++ engine/main.cpp
@@ -1,24 +1,29 @@
 #include <iostream>
 
 #include "common/typedef.h"
-#include <assert.h>
+#include "assert.h"
 
-//look up cmake tools
-#include "meConfig.h"
+#include "../builds/meConfig.h"
+
+//TODO: add (console) logger (avoiding)
+void engineStartMessage(int argc, char* argv[]){
+	if(argc < 2){
+		std::cout << "Path: "	<< argv[0] 	<< std::endl
+		<< "Version "
+			<< ME_VERSION_MAJOR << "."
+			<< ME_VERSION_MINOR << "."
+			<< ME_VERSION_PATCH
+		<< std::endl;
+	}
+}
 
 int main(int argc, char* argv[]){	
 	
-	if(argc < 2){
-		std::cout << argv[0] << " Version "
-					<< ME_VERSION_MAJOR << "."
-					<< ME_VERSION_MINOR << "."
-					<< ME_VERSION_PATCH
-					<< std::endl;
-		return 1;
-	}
+	engineStartMessage(argc, argv);
 
 	std::cout << std::endl << "[END] Press any key to close program..." << std::endl;
 	getchar();
 
 	return 0;
 }
+
