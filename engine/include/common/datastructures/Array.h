f8bb08d84c1be97a32ddfbedf0fa3a97672dec4b
Gabriel Zimmermann <gabriel.zimmermann98@gmail.com>
2019-07-02 01:02:37 +0200

    Data Structures #2 | Started: Array
    Working on array (vector) and added cmake functionality

--- engine/include/common/datastructures/Array.h
+++ engine/include/common/datastructures/Array.h
@@ -1,12 +1,29 @@
 //Array.h
 #pragma once
 
+#include "common/typedef.h"
+#include "common/assert.h"
+
+#define INIT_MAX_SIZE 10
+
+template<class T>
 class Array {
 public:
 	explicit Array();
+	explicit Array(U32 initSize);
+	explicit Array(U32 initSize, U32 maxSize);
 	virtual ~Array();
 	
+	T& get(U32 index);
+	T& first();
+	T& last();
+
+	bool isEmpty();
+	int getSize();
+	int getMaxSize();
 	
 private:
-	
+	T* array;
+	U32 size;
+	U32 maxSize;
 };
--- engine/src/common/datastructures/Array.cpp
+++ engine/src/common/datastructures/Array.cpp
@@ -1,6 +1,40 @@
 //Array.cpp
-#include "Array.h"
+#include "common/datastructures/Array.h"
 
-Array::Array() {}
-Array::~Array() {}
+template<class T>
+Array<T>::Array() {
+	maxSize = INIT_MAX_SIZE;
+	size = 0;
+	array = new T[maxSize];
+}
+
+template<class T>
+Array<T>::Array(U32 initSize) {
+	maxSize = (initSize > INIT_MAX_SIZE)? initSize : INIT_MAX_SIZE;	
+	size = initSize;
+	array = new T[maxSize];
+}
+
+template<class T>
+Array<T>::Array(U32 initSize, U32 maxSize) {
+	ASSERT(initSize < maxSize);
+	this->maxSize = maxSize;	
+	size = initSize;
+	array = new T[size];
+}
+
+template<class T>
+T& Array<T>::get(U32 index) {
+	ASSERT(index > size || index < 0);
+	
+}
+
+
+template<class T>
+Array<T>::~Array() {
+	for(size_t i = size - 1, iLen = 0; i >= iLen; i--){
+		delete array[i];
+	}
+	delete array;
+}
 
